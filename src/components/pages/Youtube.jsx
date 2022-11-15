2195918a88eb541a63deef70b8bd5f7a71ea343c
kimyihyung <kimlh3743@gmail.com>
2022-11-15 09:38:33 +0900

    “2022.11.15 Git commit -m “2022.11.15

--- src/components/pages/Youtube.jsx
+++ src/components/pages/Youtube.jsx
@@ -1,33 +1,64 @@
 import React from "react";
-import { useState, useEffect } from "react";
 import Header from "../layout/Header";
 import Contents from "../layout/Contents";
 import Footer from "../layout/Footer";
 import Title from "../layout/Title";
 import YoutubeSlider from "../include/YoutubeSlider";
 import YoutubeSearch from "../include/YoutubeSearch";
-import YoutubeCont from "../include/YoutubeCont";
 import YoutubeTag from "../include/YoutubeTag";
+import YoutubeCont from "../include/YoutubeCont";
 import Contact from "../layout/Contact";
+import { useState, useEffect } from "react";
 
 const Youtube = () => {
-  const [youtubes, setYoutube] = useState([]);
+  const [youtubes, setYoutubes] = useState([]);
+  const [random, setRandom] = useState([]);
 
   const search = async (query) => {
     await fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=AIzaSyCNlEvsy-8VtD9dAUQihHV-OxP2E9wsOn0&type=video`
+      //   `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=AIzaSyDTIcjNDrFGsQio928pinMUZrR4h9XlmGE&maxResults=30&type=video`
+      // )
+      //   .then((response) => response.json())
+      //   .then((result) => setYoutubes(result.items))
+      //   .catch((error) => console.log(error));
+
+      fetch(
+        "https://raw.githubusercontent.com/kimyihyung/reactapi/main/src/utils/youtube_video.json"
+      )
     )
       .then((response) => response.json())
-      .then((result) => setYoutube(result.items))
+      .then((result) => setYoutubes(result.items))
       .catch((error) => console.log("error", error));
   };
 
   useEffect(() => {
+    // fetch(
+    //   "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=spotv&key=AIzaSyCNlEvsy-8VtD9dAUQihHV-OxP2E9wsOn0&type=video"
+    // )
+    //   .then((response) => response.json())
+    //   .then((result) => setYoutubes(result.items))
+    //   .catch((error) => console.log("error", error));
+
     fetch(
-      "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=webstoryboy&key=AIzaSyCNlEvsy-8VtD9dAUQihHV-OxP2E9wsOn0&type=video"
+      "https://raw.githubusercontent.com/kimyihyung/reactapi/main/src/utils/youtube_video.json"
     )
       .then((response) => response.json())
-      .then((result) => setYoutube(result.items))
+      .then((result) => setYoutubes(result.items))
+      .catch((error) => console.log("error", error));
+
+    // fetch(
+    //   "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=soccer&key=AIzaSyCNlEvsy-8VtD9dAUQihHV-OxP2E9wsOn0&type=video"
+    // )
+    //   .then((response) => response.json())
+    //   .then((result) => setRandom(result.items))
+    //   // .then((result) => console.log(result.items))
+    //   .catch((error) => console.log(error));
+
+    fetch(
+      "https://raw.githubusercontent.com/kimyihyung/reactapi/main/src/utils/youtube_video.json"
+    )
+      .then((response) => response.json())
+      .then((result) => setRandom(result.items))
       .catch((error) => console.log("error", error));
   }, []);
 
@@ -35,8 +66,8 @@ const Youtube = () => {
     <>
       <Header />
       <Contents>
-        <Title title={["Youtube", "referece API"]} />
-        <YoutubeSlider youtubes={youtubes} />
+        <Title title={["YouTube", "reference API"]} />
+        <YoutubeSlider random={random} />
         <YoutubeSearch onSearch={search} />
         <YoutubeTag onSearch={search} />
         <YoutubeCont youtubes={youtubes} />
